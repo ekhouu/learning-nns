09dedef72ba233396db6b9b4ebf7aa7306d3506d
ekhouu <elkhouriela+1@gmail.com>
2026-01-01 23:45:16 -0500

    added better ngram compat w/ featurizer

--- 00recog/tests.cpp
+++ 00recog/tests.cpp
@@ -17,7 +17,45 @@ int main() {
 }
 */
 
-// TODO: implement NN as discussed using ngram input format
+// TODO: featurize
+int featurize(std::vector<std::string> &filenames, NGramConfig &ngram_config,
+              size_t max_batch, std::string batch_dir) {
+  std::map<std::string, std::map<std::string, std::vector<double>>> data;
+  std::string line;
+
+  // i know batches can be found w/ math but im going to manually do it just to
+  // be sure for now, will fix later
+  size_t batch = 0, batches = 0;
+  for (auto &filename : filenames) {
+    std::ifstream file(filename);
+    while (std::getline(file, line)) {
+      line = standardize(line);
+      data[filename][line] = ngramify(line, ngram_config);
+      batch++;
+      if (batch != 0 && batch % max_batch == 0) {
+        json j;
+        j["n_batch"] = max_batch;
+        j["batch_n"] = batches++;
+        j["data"] = data;
+        data.clear();
+        std::ofstream(batch_dir + std::to_string(batches) + "_batch.json")
+            << j.dump();
+      }
+    }
+    file.close();
+  }
+
+  if (batch % max_batch != 0) {
+    json j;
+    j["n_batch"] = batch % max_batch;
+    j["batch_n"] = batch / max_batch;
+    j["data"] = data;
+    std::ofstream(batch_dir + std::to_string(batches) + "_batch.json")
+        << j.dump();
+  }
+
+  return 1;
+}
 
 std::string DE_PATH = "./data/de.txt";
 std::string FR_PATH = "./data/fr.txt";
@@ -28,8 +66,10 @@ size_t biN = 275, triN = 225;
 NGramConfig ngramconf(biN, triN);
 
 int main() {
-  top_ngrams(LANG, ngramconf);
+  // top_ngrams(LANG, ngramconf);
   std::string NGRAMS_PATH = "out/ngrams.json";
   NGramConfig ngram_config = load_ngrams(NGRAMS_PATH);
-  size_t n = ngram_config.features;
+  // size_t n = ngram_config.features;
+
+  featurize(LANG, ngram_config, 100000, "./out/batches/");
 }
