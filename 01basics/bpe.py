d590fbe07b7e10fc4746acc690bbe3979075c2d3
ekhouu <231795918+ekhouu@users.noreply.github.com>
2026-02-03 18:21:32 -0500

    bpe.py laptop -> PC move

--- 01basics/bpe.py
+++ 01basics/bpe.py
@@ -1,15 +1,25 @@
+"""
+og bpe merge implementation
+"""
+
 import heapq
-from collections import Counter
 
 import regex as re
 
+# TODO: list by priority
+# - output format
+# - streaming
 
+
+# some observations
+# - special tokens never get merged cuz we only merge thru pretoks
 def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]):
     corpus: str
 
-    with open(input_path, 'r', encoding='utf-8') as f:
+    with open(input_path, "r", encoding="utf-8") as f:
         corpus = f.read()
 
+    fmerge = 256
     n_curr = 256
 
     working_dict = {str(bytes(i)): i for i in range(n_curr)}
@@ -53,7 +63,8 @@ def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]):
 
         tok = [int(b) for b in bytes(it.group().strip(), "utf-8")]
         n = len(tok)
-        if n == 0: continue
+        if n == 0:
+            continue
 
         tokens.extend(tok)
 
@@ -106,6 +117,8 @@ def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]):
     # (A,B) disappears - best
     # (B,R) disappears if R
 
+    merges = []
+
     while n_curr < vocab_size:
         # moved removing logic to function
         best = next_key()
@@ -135,21 +148,25 @@ def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]):
             # ALL the cases
             # if a==-1 --> a is the start of a word
             # if a==-2 --> b has already been touched
-            if a<0: continue
+            if a < 0:
+                continue
             # if  ==-2 --> a has already been touched
-            if prev[a] == -2: continue
+            if prev[a] == -2:
+                continue
             # if  !=b  --> a was touched and b was ejected
-            if post[a] != b: continue
+            if post[a] != b:
+                continue
             # this one is basically if it has changed since we last saw
             # not sure if this is possible anymore? should whiteboard
-            if tokens[a] != a_id or tokens[b] != b_id: continue
+            if tokens[a] != a_id or tokens[b] != b_id:
+                continue
 
             l = prev[a]
             r = post[b]
 
             if l >= 0:
                 k = pack(tokens[l], tokens[a])
-                occ[k] = occ.get(k,0) - 1
+                occ[k] = occ.get(k, 0) - 1
                 touched.add(k)
 
             occ[best] = occ.get(best, 0) - 1
@@ -157,7 +174,7 @@ def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]):
 
             if r >= 0:
                 k = pack(tokens[b], tokens[r])
-                occ[k] = occ.get(k,0) - 1
+                occ[k] = occ.get(k, 0) - 1
                 touched.add(k)
 
             tokens[b] = n_curr
@@ -170,21 +187,21 @@ def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]):
             if l >= 0:
                 post[l] = b
 
-            post[b] = r if r>=0 else -1
+            post[b] = r if r >= 0 else -1
             if r >= 0:
                 prev[r] = b
 
             # DO NOT MOVE THIS SECTION
             # HAS TO BE DONE AFTER EVERYTHING ELSE
             if l >= 0:
-                k = pack(tokens[l], tokens[b]) # since we already changed [b]
-                occ[k] = occ.get(k,0) + 1
+                k = pack(tokens[l], tokens[b])  # since we already changed [b]
+                occ[k] = occ.get(k, 0) + 1
                 cur.setdefault(k, []).append(b)
                 touched.add(k)
 
             if r >= 0:
                 k = pack(tokens[b], tokens[r])
-                occ[k] = occ.get(k,0) + 1
+                occ[k] = occ.get(k, 0) + 1
                 cur.setdefault(k, []).append(r)
                 touched.add(k)
 
@@ -195,6 +212,52 @@ def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]):
                 else:
                     heapq.heappush(heap, (-c, -k))
 
+        merges.append(best)
         n_curr += 1
 
-train_bpe("SPECIAL_TEST.txt",500,[])
+    # DEBUG
+    # thinking about merge structure
+    # we start from l side of merges array
+    # [12,25,39]...etc
+    # we unpack
+    # a_id, b_id = unpack(m)
+    # so example
+    # a_id, b_id = 1,2
+    # then we want to convert to bytes
+    # we can do it using the previous merges AND build dict at the same time
+
+    # check if a_id and b_id are in basic bytes dict (0-255)
+    # if yes, use bytes for remapping
+    # if not,
+    #       recursively unpack a_id and b_id until they are
+    #           add to dict so we dont have to do it again
+
+    out_merges = []
+
+    with open("debug_merge.txt", "w", encoding="utf-8") as f:
+        final_dict = {i: bytes([i]) for i in range(0, 256)}
+
+        # TODO: write into output (cuz memory overhead)
+        def bk_unpack(token_id: int) -> bytes:
+            if token_id in final_dict:
+                return final_dict[token_id]
+
+            packed = merges[token_id - fmerge]
+            a_id, b_id = unpack(packed)
+            a, b = bk_unpack(a_id), bk_unpack(b_id)
+            final_dict[token_id] = a + b
+            return final_dict[token_id]
+
+        for m in merges:
+            a_id, b_id = unpack(m)
+            out_merges.append((bk_unpack(a_id), bk_unpack(b_id)))
+            f.write(f"a_id: {a_id}, b_id: {b_id} --> {out_merges[-1]}\n")
+
+    print(working_dict)
+
+    # OUTPUT FORMAT
+    # vocab: dict[int,bytes]
+    # out_merges: list[tuple[bytes,bytes]]
+
+
+train_bpe("SPECIAL_TEST.txt", 500, [])
